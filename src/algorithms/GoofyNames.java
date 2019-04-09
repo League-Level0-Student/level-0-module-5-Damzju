a675a84aa20d41e7df4a7e55a6e2dff068d6bb9b
55378 <55378@0338S-S-R0B6ZH0.sdcs.local>
2019-04-09 14:25:23 -0700

    km

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -8,26 +8,33 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "Tobiscuit";
+		String goofyName = "";
+		String nem = JOptionPane.showInputDialog("What yo name?");
 
 		// 1. Ask the user to enter their name
-		JOptionPane.showMessageDialog(null, "What yo name?");
+
 		// 2. Print upper case name to the console using .toUpperCase()
 		// Run your program to see that this works.
-		System.out.println(goofyName.toUpperCase());
+		System.out.println(nem.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < nem.length(); i++) {
 
-		// 4. Create a String variable to store the next character of the name
-		// using .substring(start, end)
-		// HINT: replace 'start' and 'end' to get String with character at i
-
-		// 5. Use MODULO to set this variable to upper case for EVEN characters
-		// and lower case for ODD characters.
-
-		// 6. ADD this String (containing 1 char) to the goofyName String
-
-		// 7. Use pop-up to show user their Goofy name
-
+			// 4. Create a String variable to store the next character of the name
+			// using .substring(start, end)
+			// HINT: replace 'start' and 'end' to get String with character at i
+			String goof = nem.substring(0, 9);
+			// 5. Use MODULO to set this variable to upper case for EVEN characters
+			// and lower case for ODD characters.
+			if (i % 2 == 0) {
+				goof = goof.toUpperCase();
+			} else {
+				goof = goof.toLowerCase();
+			}
+			// 6. ADD this String (containing 1 char) to the goofyName String
+			goofyName += goof;
+			// 7. Use pop-up to show user their Goofy name
+			JOptionPane.showMessageDialog(null, goof);
+		}
 	}
 }
